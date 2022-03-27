6f77b1a798081d90e48fa046c4c870f6c1f60088
maythinn <90564038+developer-maythinn@users.noreply.github.com>
2022-03-27 22:10:48 +0630

    css

---
+++ webdesign/kkk/JS/day1-js(12)/data-type.js
@@ -0,0 +1,16 @@
+// let num=5;
+// let name="Hello";
+// let message=true;
+// alert( "not a number" / 2 ); // NAN
+
+let name = "hello"
+let myName = `${name} JS`;
+console.log(myName)
+
+
+let isGreater = 4 < 1;
+console.log(isGreater)
+
+console.log(typeof 0  )
+console.log(typeof true)
+console.log(typeof "Hello")
---
+++ webdesign/kkk/JS/day1-js(12)/index.js
@@ -0,0 +1,33 @@
+// console.log("JS")
+
+// let message = "Hello!";
+// console.log(message)
+
+// let name = "May", age = 20;
+// name="Khine";
+
+// const myName = "May";
+// myName="Thinn";
+// console.log(myName, age) // error
+
+// let name;
+// name="JS";
+// name="javascript";
+// console.log(name)
+
+
+// let hello = "Hello";
+// let message = hello;
+// console.log(hello, message)
+// alert('Hello')
+
+
+// if(true){
+//     let test = true;
+//     alert(test)
+// }
+"use strict";
+num = 5;
+console.log(num)
+
+
