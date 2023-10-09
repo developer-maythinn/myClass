a823397daed5d0c6500608f3a843a40492405838
maythinn <90564038+developer-maythinn@users.noreply.github.com>
2023-10-09 22:12:55 +0630

    day1-html-batch-4

---
+++ webdesign/web-design-batch-3/js/day2-js/script.js
@@ -0,0 +1,46 @@
+// console.log("Hello Word");
+// // console.log("comment");
+// console.log("multi comment");
+
+// let user = "Su Su";
+// let age = 23;
+
+// let user = "su su",
+//   age = 20;
+// const fruit = "apple";
+
+// user = "mg mg";
+// // fruit = "mango";
+// user = "Hla Hla";
+// console.log(user, age, fruit);
+
+// let username;
+// username = "Aung Aung";
+// username = "Su Su";
+// console.log(username);
+
+// let username = "Su Su";
+// let name = username;
+
+// console.log("username", username);
+// console.log("name", name);
+
+// alert(username);
+
+// let user = "John";
+// let username = "Jobs";
+// var test = "$10";
+
+// if (true) {
+//   let user = 30;
+//   var test = "$100";
+//   console.log("inner", user);
+// }
+// console.log(user);
+
+// console.log(test);
+
+"use strict";
+// num = 12;
+let num = 12;
+console.log(num);
